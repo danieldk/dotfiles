d9019f09fc7291c17b99fb8323188ca90956f0fe
Daniël de Kok <me@danieldk.eu>
2011-09-08 21:24:38 +0200

    Some sane defaults for OS X.

---
+++ bootstrap-osx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Expanded save dialog.
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expanded print dialog.
+defaults write -g PMPrintingExpandedStateForPrint -bool true
+
+# Disable automatic spelling correction
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Don't write .DS_Store files on network volumes.
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
