2d7f95864ddee9c40fa5c55ebeb74308c16d1ebf
Daniël de Kok <me@danieldk.eu>
2011-09-30 18:13:09 +0200

    Set up Sublime Text settings in OS X bootstrap.

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SUBLIME_PKGDIR=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+
 # Expanded save dialog.
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 
@@ -11,3 +13,10 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Don't write .DS_Store files on network volumes.
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+if [ -d "${SUBLIME_PKGDIR}/User" ] ; then
+  rm -rf "${SUBLIME_PKGDIR}/User"
+else
+  mkdir -p "${SUBLIME_PKGDIR}"
+fi
+
+ln -s ${PWD}/SublimeText2 "${SUBLIME_PKGDIR}/User"
