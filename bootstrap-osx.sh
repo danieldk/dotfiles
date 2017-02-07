fbdaccdd4d41b622d5153461dfad31bbc79f1697
Daniël de Kok <me@danieldk.eu>
2017-02-07 12:35:53 +0100

    Haven't been using Sublime for years.

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-SUBLIME_PKGDIR=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-
 launchctl unload -w /System/Library/LaunchAgents/com.apple.gamed.plist
 
 # Expanded save dialog.
@@ -15,14 +13,6 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Don't write .DS_Store files on network volumes.
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-if [ -d "${SUBLIME_PKGDIR}/User" ] ; then
-  rm -rf "${SUBLIME_PKGDIR}/User"
-else
-  mkdir -p "${SUBLIME_PKGDIR}"
-fi
-
-ln -s ${PWD}/SublimeText2 "${SUBLIME_PKGDIR}/User"
-
 brew install archey
 brew install z
 brew install tmux
