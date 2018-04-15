0ff8846581e494743bf2fc34927463e8bf09f810
Daniël de Kok <me@danieldk.eu>
2018-04-15 10:12:53 +0200

    Disable hyperlink auditing auditing baecon.

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -13,6 +13,11 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Don't write .DS_Store files on network volumes.
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable hyperlink auditing auditing baecon.
+# https://github.com/el1t/uBlock-Safari/wiki/Disable-hyperlink-auditing-beacon
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2HyperlinkAuditingEnabled -bool false
+
 brew install z
 brew install tmux
 brew install reattach-to-user-namespace
