9b26e6dbe79bb338feb528be9f2ae1603cdc6e05
Daniël de Kok <me@danieldk.eu>
2022-04-24 12:02:58 +0200

    Doom: use Operator Mono

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,9 +22,12 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
+(setq doom-font (font-spec :family "Operator Mono SSm" :size 16 :weight 'book))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
+;
 (setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
