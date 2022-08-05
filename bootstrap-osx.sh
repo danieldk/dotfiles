55b6559402f76aa0d4d6354f799100db2bc7e72b
Daniël de Kok <me@danieldk.eu>
2022-08-05 17:17:53 +0200

    macOS: Disable press-and-hold, remove old stuff

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-launchctl unload -w /System/Library/LaunchAgents/com.apple.gamed.plist
-
 # Expanded save dialog.
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 
@@ -14,10 +12,5 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Don't write .DS_Store files on network volumes.
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Disable hyperlink auditing auditing baecon.
-# https://github.com/el1t/uBlock-Safari/wiki/Disable-hyperlink-auditing-beacon
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2HyperlinkAuditingEnabled -bool false
-
-brew install z
-brew install tmux
-brew install reattach-to-user-namespace
+# Disable press-and-hold for accented characters.
+defaults write -g ApplePressAndHoldEnabled -bool false
