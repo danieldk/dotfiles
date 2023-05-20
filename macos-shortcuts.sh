44b58bc8e92687a9427a35b8afb8cb6aebea0c50
Daniël de Kok <me@danieldk.eu>
2023-05-20 11:35:40 +0200

    Shortcuts: also update com.apple.custommenu.apps

--- macos-shortcuts.sh
+++ macos-shortcuts.sh
@@ -7,3 +7,13 @@ defaults write com.apple.Terminal NSUserKeyEquivalents "{
     'Enter Full Screen' = '${CMD}${RETURN}';
     'Exit Full Screen' = '${CMD}${RETURN}';
 }"
+
+if defaults read com.apple.universalaccess com.apple.custommenu.apps > /dev/null 2>&1; then
+    defaults delete com.apple.universalaccess com.apple.custommenu.apps
+fi
+defaults write com.apple.universalaccess com.apple.custommenu.apps -array
+
+defaults write com.apple.universalaccess com.apple.custommenu.apps "(
+    'com.apple.Terminal',
+    NSGlobalDomain
+)"
