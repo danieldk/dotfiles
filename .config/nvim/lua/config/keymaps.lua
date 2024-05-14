df77b20517e198b2f1d529ad74adaa393d4ffb20
Daniël de Kok <me@danieldk.eu>
2024-05-14 20:57:16 +0200

    nvim: `<leader>w[sv]` for window splitting

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,5 +3,10 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 map({ "n" }, "<leader>fs", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("n", "<leader>ws", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>wv", "<C-W>v", { desc = "Split Window Right", remap = true })
+unmap("n", "<leader>w|")
+unmap("n", "<leader>w-")
