68f0540e514c3b8a037e43c2c4f629034247a207
Daniël de Kok <me@danieldk.eu>
2024-05-14 20:56:38 +0200

    nvim: use Neogit for `<leader>gs`

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+    keys = { { "<leader>gs", "<cmd>Neogit<CR>", mode = "n", desc = "Open Neogit" } },
+  },
+}
