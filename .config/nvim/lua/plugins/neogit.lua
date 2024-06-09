bda8b04f660f11bffc761e0135afe5148310a6b6
Daniël de Kok <me@danieldk.eu>
2024-06-09 10:06:03 +0200

    neogit: don't persist --reset-author

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -11,6 +11,14 @@ return {
     keys = { { "<leader>gn", "<cmd>Neogit<CR>", desc = "Open Neogit" } },
     opts = {
       kind = "auto",
+      ignored_settings = {
+        "NeogitPushPopup--force-with-lease",
+        "NeogitPushPopup--force",
+        "NeogitPullPopup--rebase",
+        "NeogitCommitPopup--allow-empty",
+        "NeogitCommitPopup--reset-author",
+        "NeogitRevertPopup--no-edit",
+      },
     },
   },
 }
